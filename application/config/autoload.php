0159f316647c1d864645d82d731a9a8f5e73e357
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2023-07-02 16:36:46 +0700

    mengganti nama file model_invoice.php menjadi Model_invoice.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('model_invoice','Product_model','model_auth','Coupon_model','User_model','account_model');
+$autoload['model'] = array('Model_invoice','Product_model','model_auth','Coupon_model','User_model','account_model');
